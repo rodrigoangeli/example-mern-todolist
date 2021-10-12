875203739c1bad510073a2308e504ff539becfd1
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2021-10-12 03:39:04 -0300

    api + client

---
+++ api/index.js
@@ -0,0 +1,65 @@
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  port = 4000;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json(), urlencodedParser);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  next();
+});
+
+const TodoItem = mongoose.model(
+  "TodoItem",
+  new mongoose.Schema({
+    text: String,
+  })
+);
+
+app.get("/item", (req, res) => {
+  TodoItem.find({}, (err, list) => {
+    res.send(list);
+  });
+});
+
+app.post("/item", (req, res) => {
+  let text = req.body.text;
+  if (text === "") return res.status(400).json({ message: "Empty field" });
+  const newItem = new TodoItem({ text });
+  newItem.save((err) => {
+    if (err) return res.status(400).send(err);
+    return res.status(200).json({
+      message: "Success",
+      newItem,
+    });
+  });
+});
+
+app.delete("/item/:id", (req, res) => {
+  TodoItem.findOneAndRemove({ _id: req.params.id }, (err) => {
+    if (err) res.status(400).send(err);
+    res.status(200).send();
+  });
+});
+
+var uri = `mongodb://localhost:27017/example?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    app.listen(port, () => {
+      console.log(`App is running on port ${port}!`);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/item")
+      .then(({ data }) => setItems(data))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const addItem = () => {
+    axios
+      .post("http://localhost:4000/item", {
+        text: inputText,
+      })
+      .then(({ data }) => {
+        setItems([...items, data.newItem]);
+        setInputText("");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const removeItem = (id) => {
+    axios
+      .delete(`http://localhost:4000/item/${id}`)
+      .then((data) => {
+        let newList = [...items];
+        setItems(newList.filter((e) => e._id !== id));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="input__wrapper">
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <span onClick={addItem}>+</span>
+      </div>
+      <ul>
+        {items.map((data) => (
+          <li key={data._id}>
+            {data.text}
+            <span onClick={(e) => removeItem(data._id)}> -</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
